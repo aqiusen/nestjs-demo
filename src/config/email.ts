c6cdfb6ea7e3d36a7b68f87165e73f77054b4d66
sqiu <sen.qiu@thoughtworks.com>
2021-12-07 21:16:17 +0800

    init code

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,8 +1,11 @@
 import { join } from 'path';
 import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
-
+/**
+ *var transporter = nodemailer.createTransport('smtps://<发送邮箱>:<授权码>@smtp.qq.com');
+ * 授权码是要在qq邮箱中开启的
+ * */
 export default {
-  transport: 'smtps://812656518@qq.com:tsjjzmqcmhmoheje@smtp.qq.com',
+  transport: 'smtps://812656518@qq.com:bhyzqjnlhxfibdcf@smtp.qq.com',
   defaults: {
     from: '"nest-modules" <modules@nestjs.com>',
   },
